7bc763629a02adc8e66138dfd4d19171a5373eac
hide-ous <mattia.samory@gmail.com>
2024-05-22 18:30:45 +0200

    * bugfix

--- one_time_scraper.py
+++ one_time_scraper.py
@@ -5,19 +5,23 @@ import pathlib
 import time
 from datetime import datetime, timedelta
 import optparse
+
+import schedule
 from pytangle.api import API, CONFIG_FILE_LOCATIONS
 
 from content_downloader import to_fname, download
 from real_time_monitor import TIMESTAMP_FORMAT
 
+logger = logging.getLogger(name='OneTimeScraper')
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch = logging.StreamHandler()
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+logger.setLevel(logging.DEBUG)
+options=None
 
 def main():
-    logger = logging.getLogger(name='OneTimeScraper')
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    ch = logging.StreamHandler()
-    ch.setFormatter(formatter)
-    logger.addHandler(ch)
-
+    global options, logger
     usage = "example usage: one_time_scraper.py --key APIKEY"
     parser = optparse.OptionParser(usage)
     parser.add_option("-f", "--file", dest="filename", default='pytangle_{}.njson'.format(
@@ -44,21 +48,34 @@ def main():
                       help="how many days in the past to scrape")
     (options, args) = parser.parse_args()
 
+    run_once()
+    job = schedule.every(options.days).days.do(run_once)
+    while True:
+        logger.debug('next run at ' + str(schedule.next_run()))
+        schedule.run_pending()
+        sleep_time = (schedule.next_run() - datetime.now()).total_seconds()
+        logger.debug('sleeping for {} seconds'.format(sleep_time))
+        time.sleep(sleep_time)
 
-    api = API(token = options.api_key)
+
+def run_once():
+    global options, logger
+    api = API(token=options.api_key)
     scrape_timestamp = time.strftime('%Y%m%d%H%M%S')
-    with open(options.filename, 'w+', encoding='utf8') as out_file:
+    with open(f'log{scrape_timestamp}.njson', 'w+', encoding='utf8') as out_file:
 
         try:
             for post in api.posts(listIds=options.lists,
-                includeHistory=True,
-                                       sortBy='date', count=-1, startDate=(datetime.utcnow()-timedelta(days=options.days)).strftime(TIMESTAMP_FORMAT),
-                                       ):
+                                  includeHistory=True,
+                                  sortBy='date', count=-1,
+                                  startDate=(datetime.utcnow() - timedelta(days=options.days)).strftime(
+                                      TIMESTAMP_FORMAT),
+                                  ):
 
                 post['scraped'] = datetime.utcnow().strftime(TIMESTAMP_FORMAT)
                 out_file.write(json.dumps(post) + '\n')
                 for media in post.get('media', []):
-                    url= media['url']
+                    url = media['url']
                     fname = to_fname(url)
                     media_fpath = os.path.join(f'media{scrape_timestamp}', fname)
                     pathlib.Path(os.path.dirname(media_fpath)).mkdir(parents=True, exist_ok=True)
